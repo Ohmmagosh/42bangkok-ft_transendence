cb1ac5220df2247060b45654dc5336b9691baf88
ohmmaagoch <ohm@42bangkok.com>
2023-11-10 21:53:17 +0700

    update: class use inherit
    update: style login

--- frontend/src/js/classElement.js
+++ frontend/src/js/classElement.js
@@ -1,5 +1,5 @@
 export class ClassElement {
-	constructor( tag, className, id, innerHTML) {
+	constructor( tag , className, id, innerHTML) {
 		this.ele = document.createElement(tag);
 		this.ele.className = className;
 		this.ele.innerHTML = innerHTML;
@@ -25,69 +25,39 @@ export class ClassElement {
 	}
 }
 
-export class LabelElement {
+export class LabelElement extends ClassElement {
 	constructor(ifor, text) {
-		this.ele = document.createElement('label');
+		super('label', '', '', text);
 		this.ele.htmlFor = ifor;
-		this.ele.innerHTML = text;
 	}
 
-	getElement() {
-		return this.ele;
-	}
 	setHtmlFor(htmlFor) {
 		this.ele.htmlFor = htmlFor;
 	}
 }
 
-export class InputElement {
+export class InputElement extends ClassElement {
 	constructor(type, className, id, placeholder) {
-		this.ele = document.createElement('input');
+		super('input', className, id, '');
 		this.ele.type = type;
-		this.ele.className = className;
-		this.ele.id = id;
 		this.ele.placeholder = placeholder;
-	}
-	getElement() {
-		return this.ele;
 	}
 	setType(type) {
 		this.ele.type = type;
-	}
-	setClassName(className) {
-		this.ele.className = className;
-	}
-	setId(id) {
-		this.ele.id = id;
 	}
 	setPlaceholder(placeholder) {
 		this.ele.placeholder = placeholder;
 	}
 }
 
-export class ButtonElement {
-	constructor(type, className, id, text, onclick) {
-		this.ele = document.createElement('button');
+export class ButtonElement extends ClassElement {
+	constructor(type, className, id, innerHTML, onclick) {
+		super('button', className, id, innerHTML);
 		this.ele.type = type;
-		this.ele.className = className;
-		this.ele.id = id;
-		this.ele.innerHTML = text;
 		this.ele.onclick = onclick;
-	}
-	getElement() {
-		return this.ele;
 	}
 	setType(type) {
 		this.ele.type = type;
-	}
-	setClassName(className) {
-		this.ele.className = className;
-	}
-	setId(id) {
-		this.ele.id = id;
-	}
-	setInnerHTML(innerHTML) {
-		this.ele.innerHTML = innerHTML;
 	}
 	setOnclick(onclick) {
 		this.ele.onclick = onclick;
--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -6,18 +6,16 @@ import {
 } from "./classElement.js";
 
 function login() {
-
+	const divContainer = new ClassElement('div', 'd-flex justify-content-center align-items-center container-sm vh-100 vw-100 position-relative', 'loginContainer', '');
+	const divRow = new ClassElement('div', 'row', 'loginRow', '');
+	const	headerText = new ClassElement('h1', 'text-center', 'loginHeader', 'Login');
 	const formElement = new ClassElement('form', 'container', 'loginForm', '');
-
 	const divEmail = new ClassElement('div', 'form-group','','');
 	const divPassword = new ClassElement('div', 'form-group', '', '');
-
 	const labelEmail = new LabelElement('inputEmail', 'Email address');
 	const inputEmail = new InputElement('email', 'form-control', 'inputEmail', 'Enter email');
-
 	const labelPassword = new LabelElement('inputPassword', 'Password');
 	const inputPassword = new InputElement('password', 'form-control', 'inputPassword', 'Password');
-
 	const buttonLogin = new ButtonElement('button', 'btn btn-primary', 'buttonLogin', 'Login');
 
 	divEmail.setAddElement(labelEmail.getElement());
@@ -26,12 +24,16 @@ function login() {
 	divPassword.setAddElement(labelPassword.getElement());
 	divPassword.setAddElement(inputPassword.getElement());
 
-
-
 	formElement.setAddElement(divEmail.getElement());
 	formElement.setAddElement(divPassword.getElement());
 	formElement.setAddElement(buttonLogin.getElement());
-	document.body.appendChild(formElement.getElement());
+
+	// divContainer.setAddElement(formElement.getElement());
+	divRow.setAddElement(headerText.getElement());
+	divRow.setAddElement(formElement.getElement());
+	divContainer.setAddElement(divRow.getElement());
+	document.body.appendChild(divContainer.getElement());
+	// document.body.appendChild(label.getElement());
 }
 
 export default login;
